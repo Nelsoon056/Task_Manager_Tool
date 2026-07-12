91c289b8ede9fdf7fcc707e19f8c2657b7aa45a1
Nelsoon056 <nelson.sandoval056@gmail.com>
2026-07-12 11:33:39 -0400

    creando la interfaz para subir tareas

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const circulo = document.getElementById("circulo");
 const info_img = document.getElementById("info_img");
 const info = document.getElementById("info");
 const info_card = document.getElementById("info_card");
+const agregar_boton = document.getElementById("agregar_boton");
+const vacio = document.getElementById("vacio");
+const basura = document.getElementById("basura");
+const agg_cerrar_boton = document.getElementById("agregar_cerrar_boton");
+const agg_card = document.getElementById("agregar_card");
 
 
 const preferencia = window.matchMedia('(prefers-color-scheme: light)').matches;
@@ -22,16 +27,22 @@ boton_tema.addEventListener('click', () => {
     if (document.body.classList.contains('modo_claro')) {
         localStorage.setItem('theme', 'claro');
         texto_tema.textContent = "Modo Claro";
+        basura.src = "assets/img/borrar_n.png"
         info_img.src = "assets/img/info_n.png"
         circulo.classList.add("mover_circulo");
     } else {
         localStorage.setItem('theme', 'oscuro');
         texto_tema.textContent = "Modo Oscuro";
+        basura.src = "assets/img/borrar.png"
         info_img.src = "assets/img/info.png"
         circulo.classList.remove("mover_circulo");
     }
 });
 
 function mostrarCard() {
-    info_card.classList.toggle("bajar")
+    info_card.classList.toggle("bajar");
+}
+
+function mostrarAgg() {
+    agg_card.classList.toggle("bajar");
 }
